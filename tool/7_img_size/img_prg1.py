f1f1114b9517db05d12e4f1c484d9d7fd991f0a7
xiaojia <seefs@163.com>
2021-07-08 14:02:03 +0800

    rename

---
+++ tool/7_img_size/img_prg1.py
@@ -0,0 +1,21 @@
+
+from PIL import Image
+ 
+filename = 'prg_1.png'
+img = Image.open(filename)
+img_array=img.load()
+imgSize = img.size 
+print (imgSize)
+maxSize = max(imgSize) 
+ 
+minSize = min(imgSize) 
+print(maxSize, minSize)
+
+print(img_array[0, 0])
+
+f = open('./array_prg.txt','w')
+for i in range(minSize):
+	for j in range(maxSize):
+		f.write(str(i)    + ','  + str(j)  + ','  + str(img_array[j, i]) + '\n')
+
+
---
+++ tool/7_img_size/test_py_install.bat
@@ -0,0 +1,12 @@
+
+::
+C:\Windows\System32\cmd.exe
+
+:: 
+python --version
+::	Python 3.6.6
+
+::
+python img_prg2.py
+
+
